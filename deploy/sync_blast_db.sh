fe6a7729a6227e9a13a05513d36fd590b48d41e8
John Xu <zhxu73@email.arizona.edu>
2019-10-27 23:20:47 -0700

    fix bash variable in sync script

--- deploy/sync_blast_db.sh
+++ deploy/sync_blast_db.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
 SYNC_PATH=$(cat /var/www/sequenceserver/irods_sync_path.txt)
-irsync -r i:SYNC_PATH /var/www/sequenceserver/db
+irsync -r i:$SYNC_PATH /var/www/sequenceserver/db
 
