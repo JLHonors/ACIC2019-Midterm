ab0b87a32abb2a2a9df754f6a982cf4551d55ba2
John Xu <zhxu73@email.arizona.edu>
2019-10-28 22:00:36 -0700

    Update spawn_wq_worker.sh

--- deploy/spawn_wq_worker.sh
+++ deploy/spawn_wq_worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$MASTER_IP=$(cat /var/www/sequenceserver/master_ip.txt)
-$MASTER_PORT=9123
+MASTER_IP=$(cat /var/www/sequenceserver/master_ip.txt)
+MASTER_PORT=9123
 /usr/bin/work_queue_worker $MASTER_IP $MASTER_PORT -P /var/www/sequenceserver/wq_password.txt
