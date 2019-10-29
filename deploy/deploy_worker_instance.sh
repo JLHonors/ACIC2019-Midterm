ffa493f2ef1178b6d684fbd639c2166a08d7eaef
John Xu <zhxu73@email.arizona.edu>
2019-10-28 18:31:57 -0700

    worker instance add wq password, anonymous iRODs user, seqserver user, systemd service

--- deploy/deploy_worker_instance.sh
+++ deploy/deploy_worker_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IRODS_USER=YOUR_USERNAME
+IRODS_USER=anonymous
 IRODS_PASS=YOUR_PASSWORD
 
 MASTER_IP=XX.XX.XX.XX
@@ -17,40 +17,55 @@ IRODS_GROUP=iplant-everyone
 #
 #######################################################
 
-if [ -z $SUDO_USER ]; then
-    echo "Need to run the script as sudo"
-    exit -1
+SEQSERVER_USER=seqserver
+SEQSERVER_GROUP=seqserver_grou
+SEQSERVER_BASE_PATH=/var/www/sequenceserver
+SEQSERVER_DB_PATH=/var/www/sequenceserver/db
+SEQSERVER_SYNC_PATH_FILE=/var/www/sequenceserver/irods_sync_path.txt
+
+if [ $USER != "root" ]; then
+    if [ -z $SUDO_USER ]; then
+        echo "Need to run the script as sudo"
+        exit -1
+    fi
 fi
-if [ "$IRODS_USER" == "YOUR_USERNAME" ]; then
-    echo "Change IRODS_USER in script"
-    exit -1
-fi
-if [ "$IRODS_PASS" == "YOUR_PASSWORD" ]; then
-    echo "Change IRODS_PASS in script"
-    exit -1
-fi
-if [ "$MASTER_IP" == "XX.XX.XX.XX" ]; then
-    echo "Change MASTER_IP in script"
-    exit -1
+if [ $IRODS_USER != "anonymous" ]; then
+    if [ $IRODS_USER == "YOUR_USERNAME" ]; then
+        echo "Change IRODS_USER in script"
+        exit -1
+    fi
+    if [ $IRODS_PASS == "YOUR_PASSWORD" ]; then
+        echo "Change IRODS_PASS in script"
+        exit -1
+    fi
 fi
 
 #
 #
 # Update & Upgrade
-sudo apt-get -qq -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y update
 
 #
 #
 # Install with apt
-sudo apt-get -qq -y install wget curl
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install wget curl
+
+#
+# Create user
+sudo addgroup $SEQSERVER_GROUP
+sudo adduser --quiet --disabled-login --gecos 'SequenceServer' $SEQSERVER_USER
+sudo adduser $SEQSERVER_USER $SEQSERVER_GROUP
+if [ -z $SUDO_USER ]; then
+    sudo adduser $SUDO_USER $SEQSERVER_GROUP
+fi
 
 #
 #
 # Store IP of WQ master into file
 cd ~/
-touch ~/master_ip.txt
-chmod 640 ~/master_ip.txt
-echo $MASTER_IP > ~/master_ip.txt
+touch $SEQSERVER_BASE_PATH/master_ip.txt
+chmod 640 $SEQSERVER_BASE_PATH/master_ip.txt
+echo $MASTER_IP > $SEQSERVER_BASE_PATH/master_ip.txt
 
 #
 #
@@ -60,6 +75,7 @@ curl -O http://ccl.cse.nd.edu/software/files/cctools-7.0.19-x86_64-centos7.tar.g
 tar -xvf cctools-7.0.19-x86_64-centos7.tar.gz
 sudo cp cctools-7.0.19-x86_64-centos7/bin/* /usr/bin/
 rm cctools-7.0.19-x86_64-centos7.tar.gz
+which work_queue_worker
 
 #
 #
@@ -69,12 +85,13 @@ curl -O ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.
 tar -xvf ncbi-blast-2.9.0+-x64-linux.tar.gz
 sudo cp ncbi-blast-2.9.0+/bin/* /usr/bin/
 rm ncbi-blast-2.9.0+-x64-linux.tar.gz
+which blastn
 
 #
 #
 # Create directory for database, and change owner to current user
 sudo mkdir -p /var/www/sequenceserver/db
-sudo chown $SUDO_USER:$IRODS_GROUP /var/www/sequenceserver/db
+sudo chown $seqserver:$seqserver /var/www/sequenceserver/db
 
 #
 # Download sample database
@@ -89,12 +106,47 @@ rm ~/vector.tar.gz
 touch ~/password.txt
 chmod 600 ~/password.txt
 chown $SUDO_USER:root ~/password.txt
-echo $IRODS_PASS > ~/password.txt
+if [ $IRODS_USER != "anonymous" ]; then
+    echo $IRODS_PASS > ~/password.txt
+fi
+
+touch $SEQSERVER_SYNC_PATH_FILE
+sudo chown $SEQSERVER_USER:$SEQSERVER_GROUP $SEQSERVER_SYNC_PATH_FILE
+sudo chmod o-rwx $SEQSERVER_SYNC_PATH_FILE
+echo "$IRODS_SYNC_PATH" > $SEQSERVER_SYNC_PATH_FILE
+
 mkdir -p ~/.irods/
 echo "{ \"irods_zone_name\": \"iplant\", \"irods_host\": \"data.cyverse.org\", \"irods_port\": 1247, \"irods_user_name\": \"$IRODS_USER\" }" > ~/.irods/irods_environment.json
-iinit < ~/password.txt
-irsync -r i:/iplant/home/$IRODS_USER/db /var/www/sequenceserver/db
 
 #
-# Launch work_queue_factory to spawn workers
-work_queue_factory $MASTER_IP 9123 -T wq -w $LEAST_WORKERS -W $MOST_WORKERS --cores=$CORE_PER_WORKER --memory=$MEM_PER_WORKER
+# Setup iRODs seqserver
+sudo -u $SEQSERVER_USER -H mkdir /home/$SEQSERVER_USER/.irods
+sudo cp ~/.irods/irods_environment.json /home/$SEQSERVER_USER/.irods/
+sudo chown $SEQSERVER_USER: /home/$SEQSERVER_USER/.irods/irods_environment.json
+sudo -u $SEQSERVER_USER -H iinit < ~/password.txt
+sudo -u $SEQSERVER_USER -H irsync -r i:$IRODS_SYNC_PATH $SEQSERVER_DB_PATH
+
+#
+# Launch systemd service
+cd ~/ACIC2019-Midterm/deploy
+echo "User=$SEQSERVER_USER" >> blast_db_sync.service
+sudo cp blast_db_sync.service /etc/systemd/system
+sudo cp blast_db_sync.timer /etc/systemd/system
+sudo cp spawn_wq_worker.service /etc/systemd/system
+sudo touch $SEQSERVER_BASE_PATH/wq_password.txt
+sudo chown $SEQSERVER_USER:$SEQSERVER_GROUP $SEQSERVER_BASE_PATH/wq_password.txt
+sudo chmod 600 $SEQSERVER_BASE_PATH/wq_password.txt
+echo $WORKQUEUE_PASSWORD > $SEQSERVER_BASE_PATH/wq_password.txt
+cp sync_blast_db.sh $SEQSERVER_BASE_PATH/
+sudo chown $SEQSERVER_USER:$SEQSERVER_GROUP $SEQSERVER_BASE_PATH/sync_blast_db.sh
+sudo chmod 750 $SEQSERVER_BASE_PATH/sync_blast_db.sh
+sudo systemctl daemon-reload
+
+
+#
+# Start DB sync service and spawn worker service
+sudo systemctl enable blast_db_sync.timer
+sudo systemctl start blast_db_sync.timer
+sudo systemctl enable spawn_wq_worker.service
+sudo systemctl start spawn_wq_worker.service
+#work_queue_factory $MASTER_IP 9123 -T wq -w $LEAST_WORKERS -W $MOST_WORKERS --cores=$CORE_PER_WORKER --memory=$MEM_PER_WORKER
